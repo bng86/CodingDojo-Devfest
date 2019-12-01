688e04a682370c039e66c7ca409554fc24868bd0
andyang <yang.bng86@gmail.com>
2019-12-01 15:15:46 +0800

    lion

--- app/src/main/java/tw/andyang/codingdojo/PokerParser.kt
+++ app/src/main/java/tw/andyang/codingdojo/PokerParser.kt
@@ -4,14 +4,21 @@ class PokerParser {
 
     companion object {
         fun parse(input: String): Poker {
-            val suitString = input.substring(0)
+            val suitString = input.substring(0,1)
             val pointString = input.substring(1)
             val suit = when (suitString) {
                 "C" -> Suit.Clubs
                 "S" ->  Suit.Spade
                 else-> throw Exception()
             }
-            return Poker(suit, 2)
+            val point = when (pointString) {
+                "A" -> 1
+                "J" -> 11
+                "Q" -> 12
+                "K" -> 13
+                else-> pointString.toInt()
+            }
+            return Poker(suit, point)
         }
     }
 
--- app/src/test/java/tw/andyang/codingdojo/PokerHandTest.kt
+++ app/src/test/java/tw/andyang/codingdojo/PokerHandTest.kt
@@ -22,4 +22,33 @@ class PokerHandTest {
     }
 
 
+    @Test
+    fun `input SA  is Spade 1`() {
+        val input = "SA"
+        val actual: Poker = PokerParser.parse(input)
+        val expected = Poker(Suit.Spade, 1)
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `input SJ  is Spade 11`() {
+        val input = "SJ"
+        val actual: Poker = PokerParser.parse(input)
+        val expected = Poker(Suit.Spade, 11)
+        Assert.assertEquals(expected, actual)
+    }
+    @Test
+    fun `input SQ  is Spade 12`() {
+        val input = "SQ"
+        val actual: Poker = PokerParser.parse(input)
+        val expected = Poker(Suit.Spade, 12)
+        Assert.assertEquals(expected, actual)
+    }
+    @Test
+    fun `input SK  is Spade 13`() {
+        val input = "SK"
+        val actual: Poker = PokerParser.parse(input)
+        val expected = Poker(Suit.Spade, 13)
+        Assert.assertEquals(expected, actual)
+    }
 }
